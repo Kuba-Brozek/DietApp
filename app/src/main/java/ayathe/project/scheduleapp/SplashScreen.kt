aaaf5387c5989bd39f877f09ede21ec5b46ddff7
Kuba-Brozek <kubabrozek9@gmail.com>
2022-02-01 18:59:43 +0100

    splash screen implemented

---
+++ app/src/main/java/ayathe/project/scheduleapp/SplashScreen.kt
@@ -0,0 +1,37 @@
+package ayathe.project.scheduleapp
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import ayathe.project.scheduleapp.registerlogin.activityreglog.LogRegActivity
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+        circularProgressBar.apply {
+
+            setProgressWithAnimation(200f, 3000)
+            progressMax = 200f
+            progressBarColor = Color.WHITE
+            progressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+            backgroundProgressBarColor = Color.GRAY
+            backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+            progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, LogRegActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
