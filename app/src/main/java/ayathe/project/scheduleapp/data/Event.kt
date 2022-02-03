7a8d92727360170d380c33e9016c218f47009a8c
Kuba-Brozek <kubabrozek9@gmail.com>
2022-02-03 18:12:00 +0100

    data class event modified

---
+++ app/src/main/java/ayathe/project/scheduleapp/data/Event.kt
@@ -0,0 +1,8 @@
+package ayathe.project.scheduleapp.data
+
+data class Event(
+    var name: String? = null,
+    var date: String? = null,
+    var description: String? = null,
+    var category: String? = null
+)
