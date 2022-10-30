09fdfd40982c22b07efada55dd4fe76c46453a48
Kuba-Brozek <kubabrozek9@gmail.com>
2022-10-30 14:34:11 +0100

    fragments added

---
+++ app/src/main/java/ayathe/project/scheduleapp/registerlogin/register/userDataInput/AfterRegistrationActivity.kt
@@ -0,0 +1,14 @@
+package ayathe.project.scheduleapp.registerlogin.register.userDataInput
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ayathe.project.scheduleapp.R
+
+class AfterRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_registration)
+
+
+    }
+}
---
+++ app/src/main/java/ayathe/project/scheduleapp/registerlogin/register/userDataInput/IntroductionFragment.kt
@@ -0,0 +1,19 @@
+package ayathe.project.scheduleapp.registerlogin.register.userDataInput
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ayathe.project.scheduleapp.R
+
+class IntroductionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction, container, false)
+    }
+}
